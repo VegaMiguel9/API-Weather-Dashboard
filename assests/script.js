97acfd42bf749a477583c78e85ec0ed580a309ce
VegaMiguel9 <112569370+VegaMiguel9@users.noreply.github.com>
2023-03-11 20:56:58 -0500

    Update script.js

--- assests/script.js
+++ assests/script.js
@@ -17,36 +17,28 @@ function FormatDay(date){
 
 
 
-//Calling function init();
 init();
 
-//Function init();
 function init(){
     //Get stored cities from localStorage
-    //Parsing the JSON string to an object
     var storedCities = JSON.parse(localStorage.getItem("cities"));
 
     // If cities were retrieved from localStorage, update the cities array to it
     if (storedCities !== null) {
         cities = storedCities;
       }
-    // Render cities to the DOM
     renderCities();
     // console.log(cities);
 }
 
 //Function StoreCities()
 function storeCities(){
-   // Stringify and set "cities" key in localStorage to cities array
   localStorage.setItem("cities", JSON.stringify(cities));
   console.log(localStorage);
 }
 
 //Function renderCities()
 function renderCities() {
-    // Clear cityList element
-    // cityList.text = "";
-    // cityList.HTML = "";
     cityList.empty();
     
     // Render a new li for each city
@@ -69,14 +61,12 @@ function renderCities() {
     };
 }   
 
-  //When form is submitted...
   $("#add-city").on("click", function(event){
       event.preventDefault();
 
     // This line will grab the city from the input box
     var city = $("#city-input").val().trim();
     
-    // Return from function early if submitted city is blank
     if (city === "") {
         return;
     }
